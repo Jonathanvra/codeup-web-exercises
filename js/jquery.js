6763e1e4386d8aa94dfe30f19957e6f47fd7ccd9
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2022-11-18 10:54:59 -0600

    mouse-jquery-exercise

--- js/jquery.js
+++ js/jquery.js
@@ -1,13 +1,32 @@
 "use strict";
 $(function() {
+
+	// Selectors!!!
+
+
 	// alert($("#codeup").html());
 	// alert($(".main").html());
 
-	$(".codeup").css("border", "1px solid red");
-	$("li").css("font-size", "20px").css("background", "yellow");
-	$("h1").css("background", "orange");
-	$("p").css("background", "orange");
-	alert($("h1").html());
+	// $(".codeup").css("border", "1px solid red");
+	// $("li").css("font-size", "20px").css("background", "yellow");
+	// $("h1").css("background", "orange");
+	// $("p").css("background", "orange");
+	// alert($("h1").html());
+	//
+	// $("h1, p, li").css("background","green");
 
-	$("h1, p, li").css("background","green");
+//	Mouse Events
+
+	$('h1').click(function(){
+		$(this).css("background", "green");
+	});
+
+	$('p').dblclick(function(){
+		$(this).css("font-size", "18px");
+	});
+	$('ul').hover(function() {
+		$(this).css('color', 'red');
+	}, function() {
+		$(this).css('color', 'blue');
+	});
 });
