a9176d749ea92faf1444163055db7dbf49aa4f96
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2022-11-18 10:29:16 -0600

    jquery selectors exercise

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,13 @@
+"use strict";
 $(function() {
-	alert( 'The DOM has finished loading!' );
+	// alert($("#codeup").html());
+	// alert($(".main").html());
+
+	$(".codeup").css("border", "1px solid red");
+	$("li").css("font-size", "20px").css("background", "yellow");
+	$("h1").css("background", "orange");
+	$("p").css("background", "orange");
+	alert($("h1").html());
+
+	$("h1, p, li").css("background","green");
 });
